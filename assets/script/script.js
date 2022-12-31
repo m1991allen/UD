075f29c63bf50fabe8aa4bfd2ad806b23a5c84dc
Allen.L <m1991allen@gmail.com>
2022-12-31 22:04:52 +0800

    web version

--- assets/script/script.js
+++ assets/script/script.js
@@ -370,11 +370,27 @@
     }, 5)
 
     var swiper = new Swiper('.swiper-setting', {
-        slidesPerView: 3,
-        spaceBetween: 30,
+        slidesPerView: 1,
+        spaceBetween: 10,
         pagination: {
             el: '.swiper-pagination',
             clickable: true,
+        },
+        breakpoints: {
+            768: {
+                slidesPerView: 1,
+                spaceBetween: 10,
+            },
+
+            1024: {
+                slidesPerView: 2,
+                spaceBetween: 10,
+            },
+
+            1300: {
+                slidesPerView: 3,
+                spaceBetween: 10,
+            },
         },
     })
 })()
